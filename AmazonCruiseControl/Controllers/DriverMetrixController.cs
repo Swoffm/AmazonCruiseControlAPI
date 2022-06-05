0a29d946858ed4be48aecbcb03d1fed2f0bdd3a0
Gavin Swofford <31296581+Swoffm@users.noreply.github.com>
2022-06-05 14:08:57 -0500

    Add files via upload

---
+++ AmazonCruiseControl/Controllers/DriverMetrixController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AmazonDriverCruiseControl.Infrastructure;
+using DriverMetrix.Adapter.BusinessEntites;
+using DriverMetrix.Adapter.Contracts;
+using DriverMetrix.Data.DBEntities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace AmazonDriverCruiseControl.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class DriverMetrixController : BaseController<IDriverMetrixAdapter>
+    {
+        //Ideally there would be translator to create a data transfer object from the business entity or move dto to the business entity
+        //private readonly IDriverCM_DriverBE translator;      
+        public DriverMetrixController(IDriverMetrixAdapter adapter) : base(adapter)
+        {
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<Driver>> GetDriverMetrixByDriverId(bool[] isCruise, string driverId)
+        {
+            Driver driver = await Adapter.ProcessDriverRoute(isCruise, driverId);
+            return driver;
+        }
+    }
+}
---
+++ AmazonCruiseControl/Infrastructure/RouteCM.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AmazonDriverCruiseControl.Infrastructure
+{
+    public class RouteCM
+    {
+        public string RouteName { get; set; }
+        public float AverageMPG { get; set; }
+        public float AverageCruiseControlTime { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
